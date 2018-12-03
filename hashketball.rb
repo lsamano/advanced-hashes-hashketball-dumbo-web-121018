8d9a98a036e1f6636c324526d736439b8d6bab17
Leizl Samano <leizl.samano@gmail.com>
2018-12-03 05:40:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,28 @@
 # Write your code here!
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals:3,
+          blocks:1,
+          slam_dunks:1
+        }
+      }
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
 
-
-
-
-
-
-
-
-
-
+      }
+    }
+  }
+end
